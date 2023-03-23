423d133bb3c89dd765b7ed0945c268365d5e5d4e
Sergii <sergii.khylko@outlook.com>
2023-03-23 13:31:36 +0200

    #3 implementation form validation for onChange event with two inputs

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -5,18 +5,32 @@ const SomeInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [wasNameInputTouched, setWasNameInputTouched] = useState(false);
 
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [wasEmailInputTouched, setWasEmailInputTouched] = useState(false);
+
   const isEnteredNameValid = enteredName.trim() !== "";
+  const isEnteredEmailValid = enteredEmail.includes("@");
+
   const isNameInputValid = !isEnteredNameValid && wasNameInputTouched;
+  const isEmailInputValid = !isEnteredEmailValid && wasEmailInputTouched;
+
+  let isFormValid = isEnteredNameValid && isEnteredEmailValid;
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   }
 
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  }
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
-    if (!isEnteredNameValid) {
+    if (!isFormValid) {
       setWasNameInputTouched(true);
+      setWasEmailInputTouched(true);
+
       return;
     }
 
@@ -29,7 +43,15 @@ const SomeInput = (props) => {
     setWasNameInputTouched(true);
   }
 
+  const emailInputLostFocusHandler = () => {
+    setWasEmailInputTouched(true);
+  }
+
   const nameInputClasses = isNameInputValid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = isEmailInputValid
     ? "form-control invalid"
     : "form-control";
 
@@ -43,10 +65,20 @@ const SomeInput = (props) => {
           onChange={nameInputChangeHandler}
           onBlur={nameInputLostFocusHandler}
           value={enteredName}/>
-        {isNameInputValid && <p className="error-text">Enter the name</p>}
+        {isNameInputValid && <p className="error-text">Name is required</p>}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="name">Enter an email</label>
+        <input
+          type="text"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputLostFocusHandler}
+          value={enteredEmail}/>
+        {isEmailInputValid && <p className="error-text">Email is required</p>}
       </div>
       <div className="form-actions">
-        <button>Send</button>
+        <button disabled={!isFormValid}>Send</button>
       </div>
     </form>
   );
